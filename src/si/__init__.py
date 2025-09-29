d019798b36a2fa4ab2385d1d120028b7cd8365ad
Bárbara Pinto Freitas <pg55693@alunos.uminho.pt>
2025-09-29 13:29:42 +0100

    .

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "João Capela"
-__credits__ = ["João Capela", "João Correia, Fernando Cruz", "Vítor Pereira"]
+__author__ = "Bárbara Freitas"
+__credits__ = ["Bárbara Freitas"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "João Capela"
-__email__ = "joao.capela@ceb.uminho.pt"
+__maintainer__ = "Bárbara Freitas"
+__email__ = "barbarafreitas2229@gmail.com"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
